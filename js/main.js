055285d0106e819947ef8594cd30f108cb9114ec
Абду <69426005+akabyl@users.noreply.github.com>
2023-10-20 13:32:15 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+  $('.slider').slick({
+    nextArrow: '<button class="slick-arrow slick-next"><img src="./image/icons/slide-arr.svg" alt="slick"></button>',
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="./image/icons/slide-arr.svg" alt="slick"></button>',
+    fade: true,
+  });
+});
